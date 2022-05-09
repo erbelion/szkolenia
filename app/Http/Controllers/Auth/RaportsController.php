1474a1ae516459eff5dd5de29514c9b8e4b937f5
janbor00 <janborgiel.jb@gmail.com>
2022-05-09 21:23:03 +0200

    Update RaportsController.php
    poprawionet zapytanie trza jeszcze wyswietlic poprawnie bo niedokonczone

--- app/Http/Controllers/Auth/RaportsController.php
+++ app/Http/Controllers/Auth/RaportsController.php
@@ -18,9 +18,9 @@ class RaportsController extends Controller
             $endDate = $_POST['End date'];
             echo "End date: " .$endDate. "<br/>";
         }
-        $query = DB::select('select price, users_count from editions where start_date = "$startDate" && end_date = "$endDate" RETURN users_count*price');
+        $query = DB::select(DB::raw("select users_count, users_count*price as income from editions where start_date > '$startDate' && and end_date < '$endDate' RETURN users_count*price"));
     }
-
+    
 }
 
 
