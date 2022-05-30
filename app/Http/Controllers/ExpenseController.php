08d9a089686baabe2c6261d2371cef51d4ee9377
janbor00 <janborgiel.jb@gmail.com>
2022-05-30 20:52:57 +0200

    Update ExpenseController.php
    z pomocą jacka się udało.
    Wydatki dla użytkownika

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Foundation\Auth\User;
 
 class ExpenseController extends Controller
 {
@@ -18,7 +19,8 @@ class ExpenseController extends Controller
     }
 
     public function index(){
-        $result = DB::select(DB::raw("SELECT DISTINCT SUBSTR(start_date,1,7) as Month, sum(price*users_count) AS Income from editions where SUBSTR(start_date,1,7)=SUBSTR(start_date,1,7) group by month"));
+        $user = auth()->user();
+        $result = DB::select(DB::raw("SELECT DISTINCT SUBSTR(start_date,1,7) as Month, sum(price) AS Income from editions INNER JOIN students ON students.edition_id=editions.id where SUBSTR(start_date,1,7)=SUBSTR(start_date,1,7) and students.id = '$user->id' group by month;"));
 
         return view('user.expenses', ['result' => $result]);
     }
